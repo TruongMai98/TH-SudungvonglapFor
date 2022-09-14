2bb7a06ff48c9b04da56cab26a0cb3775a58e9db
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-09-14 10:35:51 +0700

    su dung vong lap

---
+++ for-loop-ver-0-1.js
@@ -0,0 +1,7 @@
+let num = prompt('enter the number: ');
+let total = 0;
+let i = 1
+for ( ; i <= num; i += 1){
+    total += i;
+}
+alert(total);
---
+++ for-loop.js
@@ -0,0 +1,5 @@
+let i, text = "";
+for ( i = 0; i < 5; i ++){
+    text += 'the number is ' + i + '<br>';
+}
+document.getElementById('demo').innerHTML = text;
